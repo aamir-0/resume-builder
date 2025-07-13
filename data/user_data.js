c6e952587c53f7bee41e22d1c4dc9d533daa4a8e
temp <144443340+aamir-0@users.noreply.github.com>
2025-07-13 23:14:52 +0530

    Add user data structure and update section IDs in HTML

---
+++ data/user_data.js
@@ -0,0 +1,54 @@
+const userData = {
+  personal_info: {
+    name: "",
+    email: "",
+    phone_no: "",
+    address: "",
+    linkedin: "",
+    github: "",
+    portfolio: ""
+  },
+  education: [
+    {
+      school_name: "",
+      degree: "",
+      field_of_study: "",
+      graduation_year: "",
+      gpa: ""
+    }
+   
+  ],
+  work_experience: [
+    {
+      company_name: "",
+      job_title: "",
+      start_date: "",
+      end_date: "",
+      job_description: ""
+    }
+    
+  ],
+  projects: [
+    {
+      project_name: "",
+      project_description: "",
+      technologies_used: "",
+      project_url: "",
+      project_github: ""
+    }
+    // more entries...
+  ],
+ skills:[
+    {
+        technical_skills:"",
+        soft_skills:"",
+        languages:"",
+        certifications:""
+    }
+ ],
+  achievements: {
+    awards: [],
+    publications: [],
+    volunteer_work: []
+  }
+};
